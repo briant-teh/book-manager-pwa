662980e7d9b4b80b39f72e8cd08b0bc38e64fddf
Briant Teh <briant@tractive.com.my>
2019-07-01 16:45:42 +0800

    added navigation and about and contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
     {
-        path: '', pathMatch: 'full', redirectTo: '/home'
+        path: '', 
+        pathMatch: 'full', 
+        redirectTo: '/home'
     },
     {
-        path: 'home', component: HomeComponent
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
     }
 ];
 
